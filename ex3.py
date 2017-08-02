672761351c8f9e96150dccb8fd9bf6bb9e4dd4e1
大西瓜 <30462797+twoyue@users.noreply.github.com>
2017-08-02 16:54:34 +0800

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,30 @@
+#question
+print"I will now count my chickens:"
+
+#hens and roosters
+print "Hens",25.3+30.5/6
+print"Roosters",100-25*3%4
+
+#count eggs
+print "Now I will count the eggs:"
+
+print 3+2+1-5+4%2-1/4+6
+
+#true or false
+print"Is it true that 3+2<5-7?"
+
+print 3+2<5-7
+
+#true or false
+print"What is 3+2?",3.4+2.3
+print "What is 5-7?",5.2-7.4
+
+#conclution
+print "Oh,that's why it's False."
+
+print"How about some more."
+
+#everything
+print"Is it greater?",5.2>-2.2
+print"Is it greater or equal?",5>=-2
+print"Is it less or equal?",5<=-2
